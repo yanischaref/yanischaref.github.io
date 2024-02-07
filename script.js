b0b3a8a9c2b2a2cce5812fe23489b9a96c008fb5
yanis-charef <141965352+yanis-charef@users.noreply.github.com>
2024-02-07 22:43:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var stylePath = document.getElementById("style-link")
+const themeImg = document.getElementById("nav-appearance-img");
+var darkMode = true;
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+function changeTheme() {
+  if (darkMode) {
+    darkMode = false;
+    themeImg.style.opacity = 0.3;
+    setTimeout(() => {
+      themeImg.src = "./photos/icons/theme-icon-on.png";
+      themeImg.style.opacity = 1;
+    }, 100);
+
+    stylePath.setAttribute("href", "./styles/lightStyle.css");
+  } else {
+    darkMode = true;
+    themeImg.style.opacity = 0.3;
+    setTimeout(() => {
+      themeImg.src = "./photos/icons/theme-icon-off.png";
+      themeImg.style.opacity = 1;
+    }, 100);
+    stylePath.setAttribute("href", "./styles/darkStyle.css");
+  }
+}
